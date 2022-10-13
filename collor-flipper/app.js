36980306e2a8819ce181fe2a8cca35c2c65014f7
Roman <romchikus1987@gmail.com>
2022-10-13 20:27:03 +0300

    added jQuery

--- collor-flipper/app.js
+++ collor-flipper/app.js
@@ -1,9 +1,9 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+/* const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
-
+console.log(color);
 btn.addEventListener('click', () => {
-  /* get random number between 0 - 3 */
+  // get random number between 0 - 3 //
   const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
@@ -11,4 +11,23 @@ btn.addEventListener('click', () => {
 
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
-}
+} */
+
+// Using jQuery
+$(document).ready(() => {
+  const btn = $('#btn');
+  const color = $('.color');
+  const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+  console.log(color);
+  btn.click(() => {
+    const randomNumber = getRandomNumber();
+    $(document)[0].body.style.backgroundColor = colors[randomNumber];
+    console.log($(document)[0].body);
+
+    color[0].textContent = colors[randomNumber];
+  });
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+  }
+});
