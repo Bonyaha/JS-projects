ce8c4b29a51c51b049ff01a84edfe84cf80c3c8f
Roman <romchikus1987@gmail.com>
2022-10-20 20:07:42 +0300

    added jquery variant to counter

--- counter/app.js
+++ counter/app.js
@@ -1,9 +1,11 @@
 let count = 0;
 
-/* select value and buttons */
+// select value and buttons
 
 const value = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
 
 btns.forEach((item) => {
   item.addEventListener('click', (e) => {
@@ -26,3 +28,30 @@ btns.forEach((item) => {
     value.textContent = count;
   });
 });
+
+//jQuery variant
+/* $(document).ready(function () {
+  let count = 0;
+
+  let value = $('#value');
+  let btns = $('.btn');
+  console.log(btns);
+  btns.click((e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+      value.css('color', 'black');
+    }
+    if (count > 0) {
+      value.css('color', 'green');
+    }
+    if (count < 0) {
+      value.css('color', 'red');
+    }
+    value.text(count);
+  });
+}); */
