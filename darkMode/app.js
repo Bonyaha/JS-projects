9bbb8bf7d23d756dd0b8f206153db849304f943d
Roman <romchikus1987@gmail.com>
2022-10-24 21:21:47 +0300

    added jQuery variant

--- darkMode/app.js
+++ darkMode/app.js
@@ -3,8 +3,8 @@ const articlesContainer = document.querySelector('.articles');
 
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
-  /* OR */
-  /* document.documentElement.classList.toggle('dark-theme'); */
+  // OR
+  document.documentElement.classList.toggle('dark-theme');
 });
 
 const articlesData = articles
@@ -24,3 +24,30 @@ const articlesData = articles
   .join('');
 
 articlesContainer.innerHTML = articlesData;
+
+//jQuery
+/* const toggleBtn = $('.btn');
+const articlesContainer = $('.articles');
+console.log($('body'));
+$(document).ready(
+  toggleBtn.click(() => {
+    $('body').toggleClass('dark-theme');
+  })
+);
+
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+
+    return `<article class="post">
+  <h2>${title}</h2>
+  <div class="post-info">
+  <span>${formatDate}</span>
+  <span>${length} min read</span>
+  </div>
+  <p>${snippet}</p>
+  </article>`;
+  })
+  .join('');
+articlesContainer.html(articlesData); */
